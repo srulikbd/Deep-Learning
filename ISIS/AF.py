8f9b103b3c784145ebab302a5494c9643d315c77
srulikbd <35503583+srulikbd@users.noreply.github.com>
2020-10-28 04:51:18 +0200

    Add files via upload

---
+++ ISIS/AF.py
@@ -0,0 +1,29 @@
+# optional features: name, ssimilar names, day of week of the tweets, number of radical tweets, percentage of radical tweets
+import pandas as pd
+from sklearn import descisicon_trre, random forest
+
+
+def load_data(path):
+    isis_data = pd.load_excel(r'C:\Users\user\srulik\AF\tweets_isis_all.xlsx')
+
+
+def preprocessing():
+
+
+def train():
+
+
+def validation():
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+
+
